7e7ef3d7e549e78c8b6ef051f2bb2490d89392ec
edwinpp209 <edwinpotes2006@gmail.com>
2026-05-23 15:05:35 -0500

    Guardando lógica de base de datos antes del merge

--- src/main/java/bankingsystem/services/CuentaAhorrosServiceImpl.java
+++ src/main/java/bankingsystem/services/CuentaAhorrosServiceImpl.java
@@ -1,11 +1,12 @@
 package bankingsystem.services;
 
+import bankingsystem.Persistence.repository.CuentaAhorrosRepository;
+import bankingsystem.Persistence.repository.MovimientoRepository;
 import bankingsystem.domain.Cuenta;
 import bankingsystem.domain.CuentaAhorros;
 import bankingsystem.domain.Movimiento;
 import bankingsystem.domain.enums.TipoMovimiento;
-import bankingsystem.repository.CuentaAhorrosRepository;
-import bankingsystem.repository.MovimientoRepository;
+
 
 
 public class CuentaAhorrosServiceImpl implements CuentaAhorrosServices {
